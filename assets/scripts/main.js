1d57269bcaf0558f0a785c314ce0a2808636121f
OtnielSSilva <otniel.dss04@gmail.com>
2023-07-26 14:22:10 -0400

    Fix loading advice bug at `main.js`

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,4 +1,5 @@
 function insertMessage(text) {
+    removeLoading();
     const tagAdvice = document.querySelector('#advice-text-tag');
     const p = document.createElement('p');
     p.textContent = `"${text}"`;
@@ -29,7 +30,6 @@ function fetchAdvice() {
     showLoading();
     fetch("https://api.adviceslip.com/advice")
         .then((response) => response.json())
-        .then(removeLoading())
         .then((json) => insertMessage(json.slip.advice))
         .catch((error) => showError());
 }
